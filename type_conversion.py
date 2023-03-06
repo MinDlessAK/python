d809347f85e702b0b442b3244f9297dd954f8e06
MinDlessAK <MinDlessAK@users.noreply.github.com>
2023-03-06 18:44:58 +0530

    typeConvertion

---
+++ type_conversion.py
@@ -0,0 +1,26 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      akash
+#
+# Created:     14-01-2022
+# Copyright:   (c) akash 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+
+
+
+old_age = input("Enter your age : ")
+#new_age = old_age + 2
+#print(new_age)
+
+new_age = int(old_age) + 2
+print(new_age)
+
