b87dc4bd9c459615be86c845fcec8277839c4cea
MinDlessAK <MinDlessAK@users.noreply.github.com>
2023-03-06 18:42:16 +0530

    loops

---
+++ while_loops.py
@@ -0,0 +1,29 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      akash
+#
+# Created:     15-01-2022
+# Copyright:   (c) akash 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+i = 1
+while(i <= 5):
+   print(i)
+   i = i + 1
+i = 1
+while(i <= 5):
+   print(i * "*")
+   i = i + 1
+
+i = 5
+while(i >= 1):
+   print(i * "*")
+   i = i - 1
