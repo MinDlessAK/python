2e4ad83c033fc0afdd391d0c30d6bee920a1be29
MinDlessAK <MinDlessAK@users.noreply.github.com>
2023-03-06 18:43:01 +0530

    conditional statment

---
+++ if_else.py
@@ -0,0 +1,21 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      akash
+#
+# Created:     15-01-2022
+# Copyright:   (c) akash 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+
+age = 25
+if age >= 18:
+   print("you are an adult")
+   print("you can vote")
+elif age < 18 and age < 3:
+   print("you are a child")
+else:
+   print("you are in school")
+print("thank you")
