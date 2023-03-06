d92dd79c73eab51701d92ae87f1c28b06a60d7b1
MinDlessAK <MinDlessAK@users.noreply.github.com>
2023-03-06 18:45:23 +0530

    tuple

---
+++ tupls.py
@@ -0,0 +1,20 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      akash
+#
+# Created:     15-01-2022
+# Copyright:   (c) akash 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+marks = (95, 98, 97, 97)
+#marks[0] = 98
+print(marks.count(97))
+print(marks.index(97))
