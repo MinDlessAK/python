40d46508762fb8c7c939260640c330a1ced64ab9
MinDlessAK <MinDlessAK@users.noreply.github.com>
2023-03-06 18:43:11 +0530

    input

---
+++ input1.py
@@ -0,0 +1,18 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      akash
+#
+# Created:     14-01-2022
+# Copyright:   (c) akash 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+superhero = input("What is your superhero name?")
+print(superhero)
