e351507e3935d4f9bd26e0508906d3014fa811f1
MinDlessAK <MinDlessAK@users.noreply.github.com>
2023-03-06 18:45:55 +0530

    sum

---
+++ print_the_sum_of_two_number.py
@@ -0,0 +1,26 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      akash
+#
+# Created:     14-01-2022
+# Copyright:   (c) akash 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+first_number = input("Enter 1st number : ")
+second_number = input("Enter 2nd number : ")
+sum = float(first_number) + float(second_number)
+
+print("the sum is : " + str(sum))
+
