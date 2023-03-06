1b6e5eb83d0fc20d7626900f9bd477710cbf61f5
MinDlessAK <MinDlessAK@users.noreply.github.com>
2023-03-06 18:45:38 +0530

    input

---
+++ takingInput.py
@@ -0,0 +1,26 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      akash
+#
+# Created:     14-01-2022
+# Copyright:   (c) akash 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+first_name = "Tony"
+last_name = "Stark"
+age = 52
+is_genius = True
+name = input("What is your name? ")
+print("Hello " + name)
+print("Welcome to our cool Python class")
+print(first_name +" "+last_name)
+print (age)
+print (is_genius)
