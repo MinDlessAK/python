22d8e4834db6d623048f2b7fa9b0783a3a689d18
MinDlessAK <MinDlessAK@users.noreply.github.com>
2023-03-06 18:44:08 +0530

    module

---
+++ modules.py
@@ -0,0 +1,28 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      akash
+#
+# Created:     16-01-2022
+# Copyright:   (c) akash 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+import math
+print(dir(math))
+
+import random
+print(dir(random))
+
+import string
+print(dir(string))
+
+from math import sqrt
+print(sqrt(4))
+
