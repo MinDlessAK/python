54e9cf5bff7e02aa000f94d7572b7e6c18bbcb00
MinDlessAK <MinDlessAK@users.noreply.github.com>
2023-03-06 18:45:11 +0530

    string

---
+++ strings.py
@@ -0,0 +1,23 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      akash
+#
+# Created:     14-01-2022
+# Copyright:   (c) akash 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+name = "Tony Stark"
+print(name.upper())
+
+print(name.lower())
+print(name.find("Stark"))
+
+print(name.replace("Tony Stark", "Ironman"))
