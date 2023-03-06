3e38d82c6b10da4653f39f83d71d242a3fc57e73
MinDlessAK <MinDlessAK@users.noreply.github.com>
2023-03-06 18:44:01 +0530

    oprator

---
+++ logical_opretor.py
@@ -0,0 +1,25 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      akash
+#
+# Created:     15-01-2022
+# Copyright:   (c) akash 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+number = 2
+print(number > 3)
+print(number < 3)
+print(not number > 3)
+print(not number < 3)
+
+print(number > 3 and number > 1)
+print(number > 3 or number > 1)
+
