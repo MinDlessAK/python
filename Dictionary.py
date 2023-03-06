409a39354d76bcd14f90d76a5ab01d25ba8ad182
MinDlessAK <MinDlessAK@users.noreply.github.com>
2023-03-06 18:42:00 +0530

    dictonary

---
+++ Dictionary.py
@@ -0,0 +1,25 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      akash
+#
+# Created:     15-01-2022
+# Copyright:   (c) akash 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+marks = {"math" : 99, "chemistry" : 98, "physics" : 97}
+print(marks)
+print(marks["chemistry"])
+
+marks["english"] = 95
+print(marks)
+
+marks["math"] = 96
+print(marks)
